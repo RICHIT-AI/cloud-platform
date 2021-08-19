950aab99034d75b55a6a2126f430404735a8de6c
Francisco Garcia <fjgarciamacias@gmail.com>
2021-08-19 15:15:49 -0500

    [feat] Variables

---
+++ 0_variables.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+  description = "Project Name"
+}
+
+variable "credentials" {
+  description = "Credentials"
+}
+
+variable "project_id" {
+  description = "Project ID"
+}
+
+variable "region" {
+  description = "Region"
+}
+
+variable "zone" {
+  description = "Zone"
+}
