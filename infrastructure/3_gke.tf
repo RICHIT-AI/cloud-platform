d1ec1b5779832896bb2458bbfec6c2514bfffc36
Francisco Garcia <fjgarciamacias@gmail.com>
2021-09-14 14:37:12 -0500

    [new] Add new pool

--- infrastructure/3_gke.tf
+++ infrastructure/3_gke.tf
@@ -36,3 +36,31 @@ resource "google_container_node_pool" "primary_nodes" {
         create_before_destroy = true
     }
 }
+
+resource "google_container_node_pool" "primary_nodes" {
+    name = "${google_container_cluster.primary.name}-node-pool2"
+    location = var.gke_location
+    cluster = google_container_cluster.primary.name
+    node_count = var.gke_num_nodes
+
+    node_config {
+        oauth_scopes = [
+            "https://www.googleapis.com/auth/logging.write",
+            "https://www.googleapis.com/auth/monitoring",
+        ]
+
+        labels = {
+            env = var.project_name
+        }
+
+        machine_type = "n1-standard-1"
+        tags = ["gke-node", "${var.project_name}-gke"]
+        metadata = {
+            disable-legacy-endpoints = "true"
+        }
+    }
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
